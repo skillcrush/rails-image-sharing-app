0a90910887db909876bbc74566ead3a662a9b4b2
Ifat Ribon <Ifat Ribon>
2016-07-23 17:17:59 -0500

    rspec tests and prettier urls

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 RSpec.describe PinsController do
 
+describe "GET index" do
+it "renders the index template" do
+get :index
+expect(response).to render_template("index")
+end
+
+it "populates an instance variable @pins with all pins in the database" do
+get :index
+expect(assigns[:pins]).to eq(Pin.all)
+end
+
+end
+
 end
