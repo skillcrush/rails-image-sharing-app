d045b7693779c82b247fc21520ec09bf3c5a83ab
inveterateliterate <iribon9@gmail.com>
2016-08-23 14:56:10 -0500

    Update create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-         @user.authenticate(params[:email], params[:password])
+         @user.authenticate
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
