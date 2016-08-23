6ed39be5166630e754389c7be6585c7db74046fd
inveterateliterate <iribon9@gmail.com>
2016-08-23 15:40:42 -0500

    Authenticate upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-         @user.authenticate(@user.email)
+         @user.current_user
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
